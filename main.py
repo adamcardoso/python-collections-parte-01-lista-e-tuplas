13aa8c5adbae54994abdcafc49d6e793d6041ffb
adamcardoso <adamjuniorcardoso@gmail.com>
2023-01-31 20:25:25 -0300

    Mais operações em listas e list comprehensions

--- main.py
+++ main.py
@@ -11,3 +11,28 @@ print('Essa é a lista de idades: ', idades)
 
 idades.clear()
 print('Essa é a lista de idades: ', idades, ' e ela está vazia')
+
+idades = [39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+print('Essa é a lista de idades: ', idades)
+
+if 39 in idades:
+    print('A idade 39 está na lista de idades')
+else:
+    print('A idade 39 não está na lista de idades')
+
+if 55 not in idades:
+    idades.append(55)
+    print('A idade 55 foi adicionada na lista de idades')
+    print(idades)
+else:
+    print('A idade 55 já está na lista de idades')
+
+idades.extend([56, 57, 58, 59, 60])
+print('Essa é a lista de idades: ', idades)
+
+idades_no_ano_que_vem = []
+
+for idade in idades:
+    idades_no_ano_que_vem.append(idade + 1)
+print('Essa é a lista de idades no ano que vem: ', idades_no_ano_que_vem)
+
